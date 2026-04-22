7838ca2de2688c81f7e0cde291c71715330c9096
cst10sites <mrkam.archive@gmail.com>
2026-04-21 23:59:23 -0700

    Add files via upload

---
+++ zonghan/project/script.js
@@ -0,0 +1,41 @@
+
+// Example 2
+function checkLang() {
+    var correctAnswer = document.getElementById("correctLang").checked;
+    // alert(correctAnswer); true or false
+    
+    if (correctAnswer == true) {
+        alert("Correct");
+    }
+    else {
+        alert("Incorrect");
+    }
+    
+}
+
+function shareholders() {
+    var correctAnswer = document.getElementById("correctLang1").checked;
+    // alert(correctAnswer); true or false
+    
+    if (correctAnswer == true) {
+        alert("Correct");
+    }
+    else {
+        alert("Incorrect");
+    }
+    
+}
+
+
+function stock() {
+    var correctAnswer = document.getElementById("correctLang2").checked;
+    // alert(correctAnswer); true or false
+    
+    if (correctAnswer == true) {
+        alert("Correct");
+    }
+    else {
+        alert("Incorrect");
+    }
+    
+}
